dc80251176c6151b291b368c88fbeab9bd1765b5
emily <nakanodepaula@gmail.com>
2022-09-13 22:49:31 -0300

    feat: encrypt password

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,9 +1,14 @@
+import bcrypt from 'bcrypt'
+
 import { User } from '@prisma/client';
 
 type CreateUser = Omit<User, "id">;
 
 export async function createUser(dataUser:CreateUser) {
+    const { email, password } = dataUser;
     
+    const passwordHash = bcrypt.hashSync(password, 10);
+  
 }
 
 export async function loginUser(dataUser:CreateUser) {
