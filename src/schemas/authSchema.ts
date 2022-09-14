9745a6ed06d220841345039cff5fc9cc09448ceb
emily <nakanodepaula@gmail.com>
2022-09-13 22:25:30 -0300

    feat: password must be 10 characters long

--- src/schemas/authSchema.ts
+++ src/schemas/authSchema.ts
@@ -2,7 +2,7 @@ import joi from 'joi';
 
 const createUserSchema = joi.object({
     email: joi.string().email().required(),
-    password: joi.string().required(),
+    password: joi.string().min(10).required(),
     confirmPassword: joi.any().equal(joi.ref('password'))
     .required()
     .label('Confirm password')
