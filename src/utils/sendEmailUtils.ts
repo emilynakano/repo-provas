ce693a9e6d516a404cd78844ec1e7c7a2f1f78f2
emily <nakanodepaula@gmail.com>
2022-09-18 17:30:49 -0300

    feat: send email to all users registreds

--- src/utils/sendEmailUtils.ts
+++ src/utils/sendEmailUtils.ts
@@ -1,27 +1,27 @@
 import sgMail from '@sendgrid/mail';
 import dotenv from 'dotenv';
 import { getUsers } from '../repositories/authRepository';
+import { getTestFromId } from '../repositories/testRepository';
 
 dotenv.config();
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY as string);
 
 export async function sendEmail(id: number) {
-  console.log(id)
+  const test = await getTestFromId(id);
   const users = await getUsers();
 
   const msg = {
     from: process.env.SENDGRID_EMAIL as string,
-    subject: 'Nova prova adicionada',
-    text: 'A seguinte prova foi adicionadas: Fulano P1 2022 - globo.com (CSS)',
-    html: '<strong>A seguinte prova foi adicionadas: Fulano P1 2022 - globo.com (CSS)</strong>',
+    subject: 'Confira a nova prova adicionada ao sistema!',
+    text: `A seguinte prova foi adicionada: ${test?.teachersDiscipline.teacher.name} ${test?.category.name} ${test?.name} (${test?.teachersDiscipline.discipline.name})`,
+    html: `<strong>A seguinte prova foi adicionada: ${test?.teachersDiscipline.teacher.name} ${test?.category.name} ${test?.name} (${test?.teachersDiscipline.discipline.name})</strong>`,
   }
 
   users.forEach(user => {
     sgMail
     .send({...msg, to: user.email})
     .then(() => {
-      console.log('Email sent')
     })
     .catch((error) => {
       console.error(error)
